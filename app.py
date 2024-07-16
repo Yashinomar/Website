b8a899e6d8823ba62ba10b0e842f31e380598a5f
Yadu <yadoukamado@gmail.com>
2024-07-16 18:53:20 +0530

    change

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-import mysql.connector
-mydb = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd='password0'
-)
+#import mysql.connector
+#mydb = mysql.connector.connect(
+#    host='localhost',
+#    user='root',
+#    passwd='password0'
+#)
 
 app = Flask(__name__)
 @app.route('/')
@@ -13,6 +13,12 @@ def index():
 @app.route('/register')
 def Signin():
     return render_template('register.html')
+@app.route('/base')
+def base():
+    return render_template('base.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 @app.route('/home')
 def home():
     return render_template('home.html')
