c2af36e7366860c5e53c24ade6c69c6407f8864a
Yashinomar <yashinomarn@gmail.com>
2024-07-16 13:26:36 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import mysql.connector
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='password0'
+)
+print(mydb)
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -7,4 +14,4 @@ def index():
 def Signin():
     return render_template('register.html')
 if __name__ == "main":
-    app.run()
+    app.run(debug=True)
